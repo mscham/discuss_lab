4fa94c4a47f57bbd0988de1d8983be80d4008705
Michael Schamberger <michael@Michaels-MBP.hsd1.ga.comcast.net>
2018-07-20 12:25:48 -0400

    first commit

---
+++ web/channels/user_socket.ex
@@ -0,0 +1,16 @@
+defmodule Discuss.UserSocket do
+  use Phoenix.Socket
+
+  ## Channels
+  channel "comments:*", Discuss.CommentsChannel
+
+  ## Transports
+  transport :websocket, Phoenix.Transports.WebSocket
+  
+  def connect(_params, socket) do
+    {:ok, socket}
+  end
+
+  def id(_socket), do: nil
+  
+end
---
+++ web/static/js/socket.js
@@ -0,0 +1,38 @@
+
+import {Socket} from "phoenix"
+
+let socket = new Socket("/socket", {params: {token: window.userToken}})
+
+socket.connect()
+
+const createSocket = (topicId) => {
+  // Now that you are connected, you can join channels with a topic:
+  let channel = socket.channel(`comments:${topicId}`, {});
+  channel.join()
+    .receive("ok", resp => { 
+     renderComments(resp.comments) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+    document.querySelector('button').addEventListener('click', () => {
+      const content = document.querySelector('textarea').value;
+
+      channel.push('comment:add', { content: content });
+    }); 
+}
+
+function renderComments(comments) {
+
+  const renderedComments = comments.map(comment => {
+    return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+    `;
+  });
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
+
+
+window.createSocket = createSocket;
+
